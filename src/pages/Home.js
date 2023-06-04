427ea096f5726e1d6dc197da2f407b5e187c36ca
Vinay <vinaybhardwaj2334@gmail.com>
2023-06-04 14:43:47 +0530

    Fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,7 +7,7 @@ import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
   const [exercises, setExercises] = useState([]);
-  const [bodyPart, setBodyPart] = useState('all');
+  const [bodyPart, setBodyPart] = useState('all'); 
 
   return (
     <Box>
